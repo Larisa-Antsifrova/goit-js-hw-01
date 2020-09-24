7c3795ecf5171caf8953c2a4f98e26daa49ca992
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-09-24 13:34:42 +0300

    Added solution to the first task

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+console.log("Задание 1");
+
+let name = "Генератор защитного поля";
+let price = 1000;
+let firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+
+console.log(firstMessage);
+
+price = 2000;
+let secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
+
+console.log(secondMessage);
